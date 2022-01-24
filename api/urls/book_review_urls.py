9a9376491029bf41049601d65550d7f9d94098ee
wenyu-chen <10756027@ntub.edu.tw>
2022-01-24 15:20:20 +0800

    feat(book):get_all comment

--- api/urls/book_review_urls.py
+++ api/urls/book_review_urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from api.views.book_review_views import *
 
-
 app_name = 'book_review'
 
 urlpatterns = [
     path('add/', add_review, name='add'),
     path('edit/<int:pk>/', edit_review, name='edit'),
+    path('all/', get_all_reviews, name='all'),
 ]
